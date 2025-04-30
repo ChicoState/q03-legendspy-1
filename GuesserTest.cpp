459d4e61efaa33778702ece3675654e9bd42b3fe
Edgar Rangel <rangeledgar136@gmail.com>
2025-04-30 13:37:20 -0700

    Add files via upload
    added tests

--- GuesserTest.cpp
+++ GuesserTest.cpp
@@ -5,6 +5,7 @@
 #include <gtest/gtest.h>
 #include "Guesser.h"
 
+
 class GuesserTest : public ::testing::Test
 {
 	protected:
@@ -14,9 +15,64 @@ class GuesserTest : public ::testing::Test
 		virtual void TearDown(){} //clean up after each test, (before destructor)
 };
 
-// Example "smoke test" (can be deleted)
-TEST(GuesserTest, smoke_test)
-{
-  Guesser object("Secret");
-  ASSERT_EQ( 1+1, 2 );
+
+TEST(GuesserTest, match){
+	Guesser g("correct");
+	EXPECT_TRUE(g.match("correct"));
+}
+
+TEST(GuesserTest, wrong){
+	Guesser g("correct");
+	EXPECT_FALSE(g.match("wrong"));
+}
+
+TEST(GuesserTest, maxSeceret_GuessOver32){
+	Guesser g("maxsecretupto32characters1234567");
+	EXPECT_TRUE(g.match("maxsecretupto32characters1234567890overmax"));
+}
+
+TEST(GuesserTest, limit){
+	Guesser g("limit");
+	g.match("fail");
+	g.match("fail");
+	g.match("fail");
+	EXPECT_FALSE(g.match("limit"));
+}
+
+TEST(GuesserTest, noPW){
+	Guesser g("");
+	EXPECT_TRUE(g.match(""));
+}
+
+TEST(GuesserTest, remaining){
+        Guesser g("fire");
+	g.match("nope");
+	g.match("nah");
+	ASSERT_EQ( g.remaining(), 1);
+}
+
+TEST(GuesserTest, distance){
+        Guesser g("farfaraway");
+        g.match("farfarawaY");
+	g.match("notevenclose");
+	g.match("FARfaraway");
+	EXPECT_TRUE(g.match("farfaraway"));
+}//there might be a bug as match checks if distance is >2 and it locks if it is
+
+TEST(GuesserTest, distance1){
+        Guesser g("minecraft");
+	g.match("Minecraft");
+        EXPECT_TRUE(g.match("minecraft"));
+}
+
+TEST(GuesserTest, distance2){
+        Guesser g("shouldfail");
+	g.match("shouldFAil");
+        EXPECT_FALSE(g.match("shouldfail"));
+}//there is bug/error/unintended 
+
+TEST(GuesserTest, remainingOnPass){
+        Guesser g("pass");
+        g.match("pass");
+	ASSERT_EQ(g.remaining(), 3);
 }
